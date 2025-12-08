f4d30dd94372d1c0e90b8f2235657857c8932529
Maebara505 <darvoxtupapi@gmail.com>
2025-12-07 23:15:26 -0500

    dañando

--- src/main/java/com/exposicion/loginmvc/controller/LoginController.java
+++ src/main/java/com/exposicion/loginmvc/controller/LoginController.java
@@ -7,13 +7,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+////////
 @Controller
 public class LoginController {
-
+/////////
     @Autowired
     private AuthService authService;
-
+////////////
     @GetMapping({"/", "/login"})
     public String showLoginForm(Model model) {
         return "login"; // Vista login.html
@@ -42,5 +42,3 @@ public class LoginController {
         return "home";
     }
 }
-
-//SEXOSEXITO UWU
