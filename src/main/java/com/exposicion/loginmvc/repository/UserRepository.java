3f08023548ced05b73811aad1b07f3b6b051d062
ldalomoto <ldalomoto@uce.edu.ec>
2025-12-08 00:35:16 -0500

    comentarios borrados

--- src/main/java/com/exposicion/loginmvc/repository/UserRepository.java
+++ src/main/java/com/exposicion/loginmvc/repository/UserRepository.java
@@ -4,22 +4,13 @@ import com.exposicion.loginmvc.model.User;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
-/**
- * Repositorio encargado del acceso a datos. Por ahora, simula la conexión a una
- * BD.
- */
+
 @Repository
 public class UserRepository {
 
     // Simulación del "usuario maestro" en la base de datos
     private final User adminUser = new User(1L, "admin", "password123");
 
-    /**
-     * Simula la búsqueda de un usuario por su nombre en la BD.
-     *
-     * @param username El nombre de usuario a buscar.
-     * @return Un objeto Optional<User> con el usuario si es encontrado.
-     */
     public Optional<User> findByUsername(String username) {
 
         // Simulación: Si el nombre de usuario coincide con el predefinido, devuelve el objeto User.
